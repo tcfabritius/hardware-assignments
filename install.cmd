af9e1c14ba7a60dc4e39a78c820b7289728f0746
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2023-10-08 21:04:26 +0300

    Improved windows install script

--- install.cmd
+++ install.cmd
@@ -1,4 +1,7 @@
 start python -m http.server
-mpremote mip install --target / http://localhost:8000/
+@rem Extract comport name where pico is connected
+for /f "tokens=1 delims= " %%a in ('mpremote connect list ^| find "2e8a:0005"') do set comport=%%a
+@rem Run mpremote
+mpremote connect %comport% mip install --target / http://localhost:8000/
 @rem The following line terminates all processes with "python" and "http.server" in them.
 wmic process where "name like '%%python%%' and commandline like '%%http.server%%'" delete
