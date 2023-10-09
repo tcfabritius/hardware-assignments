66137890626508486a6e923aaa577654f8adbf58
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2023-10-09 08:55:06 +0300

    Added python3.12 detection to OSX

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # OSX and Linux have python3 with different names.
+
 case "$(uname)" in
 
    Darwin*)
      #for OSX use python
-     python -m http.server &
+     if [ "$(which python3.12 2>/dev/null | wc -l)" -gt "0" ] ; then
+        python=python3.12
+	 else
+        python=python
+     fi
      ;;
 
    MINGW*)
      #for MINGW use python launcher
-     py -m http.server &
+     python=py
      ;;
 
    Linux*)
      #for Linux use python3
-     python3 -m http.server &
+     python=python3
      ;;
 esac
-
-comport=`mpremote connect list | grep 2e8a:0005 | cut -d' ' -f1`
-mpremote connect $comport mip install --target / http://localhost:8000/
+$python -m http.server &
+comport=`$python -m mpremote connect list | grep 2e8a:0005 | cut -d' ' -f1`
+$python -m mpremote connect $comport mip install --target / http://localhost:8000/
 kill $!
 #pkill -f http.server
