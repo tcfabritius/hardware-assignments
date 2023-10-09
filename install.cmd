f7d488270dcd9a9dd77af551b92e9524b4d2aba7
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2023-10-09 20:05:36 +0300

    Moved web server start earlier and added a delay before running install

--- install.cmd
+++ install.cmd
@@ -1,9 +1,10 @@
 @echo off
+start python -m http.server
 @rem Extract comport name where pico is connected
 for /f "tokens=1 delims= " %%a in ('python -m mpremote connect list ^| find "2e8a:0005"') do set comport=%%a
 echo Device: %comport%
-start python -m http.server
+timeout /t 2 /nobreak
 @rem Run mpremote
 python -m mpremote connect %comport% mip install --target / http://localhost:8000/
-@rem The following line terminates all processes with "python" and "http.server" in them.
-wmic process where "name like '%%python%%' and commandline like '%%http.server%%'" delete
+@rem The following line terminates all processes with "python" their name.
+wmic process where "name like '%%python%%'" delete
