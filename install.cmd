cdf833d381d284770cadc7a101c5f8d5b19f7d22
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2023-10-11 09:59:32 +0300

    Changed wmic to taskkill and added title for local webserver

--- install.cmd
+++ install.cmd
@@ -1,10 +1,10 @@
 @echo off
-start python -m http.server
+start "mpremote.webserver" python -m http.server
 @rem Extract comport name where pico is connected
 for /f "tokens=1 delims= " %%a in ('python -m mpremote connect list ^| find "2e8a:0005"') do set comport=%%a
 echo Device: %comport%
 timeout /t 2 /nobreak
 @rem Run mpremote
 python -m mpremote connect %comport% mip install --target / http://localhost:8000/
-@rem The following line terminates all processes with "python" their name.
-wmic process where "name like '%%python%%'" delete
+@rem The following line terminates all processes with mpremote.webserver as the window title.
+taskkill /fi "WINDOWTITLE eq mpremote.webserver"
