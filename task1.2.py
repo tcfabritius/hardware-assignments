41b8f7cfa9ff4115f32fcf38a24f6eee225a2c50
Chum <laakmik@hotmail.fi>
2025-03-21 09:27:18 +0200

    Second task done.

--- task1.2.py
+++ task1.2.py
@@ -1,45 +1,26 @@
 from ssd1306 import SSD1306_I2C
-from machine import UART, Pin, I2C, Timer, ADC
-import fifo
+from machine import Pin, I2C
 import time
-import micropython
-micropython.alloc_emergency_exception_buf(200)
 
-button = Pin(9, Pin.IN, Pin.PULL_UP)
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
-oled_width = 128
-oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
-rb = fifo.Fifo(50)
-listOfThings =[]
+oled = SSD1306_I2C(128, 64, i2c)
 
-if rb.empty():
-    print('Fifo is empty')
+yPos = 0  
+fontSize = 8
 
-led = Pin("LED", Pin.OUT)
 oled.fill(0)
+oled.show()
 
 while True:
-    givenInput = input("Type something will ya\nSomething: ")
-    oled.text(givenInput,1,1,1)
-    oled.show()
-    oled.scroll(0,10)
-    oled.rect(1,1,8,8,0,True)
-    time.sleep(1)
+    givenInput = input("Type something will ya: ")
+    
+    if yPos + fontSize > 64:
+        oled.scroll(0, -fontSize)
+        oled.fill_rect(0, 54, 128, 10, 0)
+        yPos = 54
+    else:
+        yPos += fontSize
 
-# if rb.empty():
-#     print('Fifo is empty')
-# 
-# led = Pin("LED", Pin.OUT)
-# oled.fill(0)
-# 
-# while True:
-#     givenInput = input("Type something will ya\nSomething: ")
-#     listOfThings.append(givenInput)
-#     #print(listOfThings)
-#     for i in listOfThings:
-#         oled.text(i,1,1,1)
-#         oled.show()
-#         oled.scroll(0,10)
-#         time.sleep(1)
+    oled.text(givenInput, 0, yPos)
+    oled.show()
 
