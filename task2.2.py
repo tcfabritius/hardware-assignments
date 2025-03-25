24e2e07b67d05a1a2c997f52719644ac42c205dc
Chum <laakmik@hotmail.fi>
2025-03-25 14:25:32 +0200

    Added 2.2

---
+++ task2.2.py
@@ -0,0 +1,26 @@
+from ssd1306 import SSD1306_I2C
+from machine import Pin, I2C
+import time
+
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+yPos = 0  
+fontSize = 8
+
+oled.fill(0)
+oled.show()
+
+while True:
+    givenInput = input("Type something will ya: ")
+    
+    if yPos + fontSize > 64:
+        oled.scroll(0, -fontSize)
+        oled.fill_rect(0, 54, 128, 10, 0)
+        yPos = 54
+    else:
+        yPos += fontSize
+
+    oled.text(givenInput, 0, yPos)
+    oled.show()
+
