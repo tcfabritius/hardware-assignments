6236b1fc8e36de6fd1fcb93ff0057a5f2d184c22
Chum <laakmik@hotmail.fi>
2025-04-08 13:53:26 +0300

    Added placeholder-logic.

--- task4.2.py
+++ task4.2.py
@@ -1,11 +1,11 @@
-from ssd1306 import SSD1306_I2C
-from led import Led
-from machine import Pin, I2C
 import time
-import micropython
 from fifo import Fifo
 
 micropython.alloc_emergency_exception_buf(200)
 
 rb = fileFifo.Fifo(50, name='capture02_250Hz.txt')
+
+for _ in range (100):
+    if rb.has_data():
+        print(rb.get())
 
